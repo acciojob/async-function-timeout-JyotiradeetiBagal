6055682b3f21986c4438061426b42270a41c57fb
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-26 19:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+// JavaScript Code
+
+const delayMessage = (message, delay) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(message);
+    }, delay);
+  });
+};
+
+document.getElementById("btn").addEventListener("click", async (event) => {
+  event.preventDefault(); 
+	
+  const text = document.getElementById("text").value;
+  const delay = parseInt(document.getElementById("delay").value, 10);
+  const outputDiv = document.getElementById("output");
+
+  outputDiv.textContent = "";
+
+  const message = await delayMessage(text, delay);
+  outputDiv.textContent = message;
+});
